8dc78789088e4964ffb402ca40bb3333855fe03d
Russell Smith <resmithjr88@gmail.com>
2022-10-01 07:29:49 -0600

    Remove method and helper functions added.

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -71,4 +71,76 @@ class BinarySearchTree {
       throw new Error('Key Not Found')
     }
   }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin()
+        this.key = successor.key
+        this.value = successor.value
+        successor.remove(successor.key)
+      }
+      /* If the node only has a left child,
+         then you replace the node with its left child. */
+      else if (this.left) {
+        this._replaceWith(this.left)
+      }
+      /* And similarly, if the node only has a right child,
+         then you replace the node with its right child. */
+      else if (this.right) {
+        this._replaceWith(this.right)
+      }
+      /* If the node has no children, then
+      simply remove it and any references to it
+      by calling `this._replaceWith(null)`. */
+      else {
+        this._replaceWith(null)
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key)
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key)
+    }
+    else {
+      throw new Error('Key Not Found')
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node
+      }
+
+      if (node) {
+        node.parent = this.parent
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key
+        this.value = node.value
+        this.left = node.left
+        this.right = node.right
+      }
+      else {
+        this.key = null
+        this.value = null
+        this.left = null
+        this.right = null
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this
+    }
+    return this.left._findMin()
+  }
 }
